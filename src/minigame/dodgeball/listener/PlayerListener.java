9ee870f595442458effa98cc90dd7faf8e8b8d2f
Loony <archie.irving@me.com>
2016-10-20 18:09:19 +0100

    Adds PAE

--- src/minigame/dodgeball/listener/PlayerListener.java
+++ src/minigame/dodgeball/listener/PlayerListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAnimationEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -20,6 +21,12 @@ public class PlayerListener  implements Listener {
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onQuit(PlayerQuitEvent event) {
 		DBPlayer.get(event.getPlayer()).unload();
+	}
+	
+	@EventHandler
+	public void onThrow(PlayerAnimationEvent event) {
+		
+		
 	}
 
 }
